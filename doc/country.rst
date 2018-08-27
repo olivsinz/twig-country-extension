e08a4acefd4f69c9085c6aaa35b011536b58ad8b
OLIVERS <zinsouoliviers@gmail.com>
2018-08-27 12:28:03 +0100

    First release

---
+++ doc/country.rst
@@ -0,0 +1,21 @@
+The Country Extension
+==================
+
+The Country extension provides the following filters:
+
+* ``country``
+
+Installation
+------------
+
+
+Showing country name based on country code
+-------------------------------------------------------
+
+Use the ``country`` filter to showing country name based on country code.
+
+.. code-block:: jinja
+
+    {{ "PL"|country }}
+
+This example would output ``Poland``, as ``PL`` is the country code.
---
+++ src/CountryExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+use Symfony\Component\Intl\Intl;
+
+/**
+ *
+ * This Twig extension to convert two letter country to country name.
+ *
+ * See https://symfony.com/doc/current/cookbook/templating/twig_extension.html
+ */
+class CountryExtension extends AbstractExtension
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return array(
+            new TwigFilter('country', array($this, 'countryFilter')),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function countryFilter($countryCode, $displayLocale = null)
+    {
+        return Intl::getRegionBundle()->getCountryName($countryCode, $displayLocale);
+    }
+}
